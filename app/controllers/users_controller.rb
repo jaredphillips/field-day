4fa6bae025c79a25ebd5c7233ebb376ee7ff2bcc
Jared Phillips <jaredtphillips@me.com>
2014-08-28 18:45:23 -0400

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
+  before_filter :load_user, except: [:new, :create]
+
   def new
+    @user = User.new
+  end
+
+  def show
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Field Day Terrariums Welcomes You!"
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   def update
@@ -10,6 +23,7 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
   def edit
   end
 
@@ -17,5 +31,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  # Commented out for now cause no new signup for site is required but maybe in the future
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
   end
 end
